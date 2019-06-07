acd604a299afaf43319ae3fe570071a07c0892f1
sio <siojurnalispipin@hotmail.com>
2019-06-07 22:47:07 +0700

    Templating admin lte2

--- application/views/dashboard/template_dashboard.php
+++ application/views/dashboard/template_dashboard.php
@@ -5,8 +5,15 @@
     <?php $this->load->view('dashboard/_partials/head');?>
 </head>
 
-<body>
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <?php $this->load->view('dashboard/_partials/main-header');?>
+        <?php $this->load->view('dashboard/_partials/main-sidebar');?>
+        <?php $this->load->view('dashboard/_partials/content');?>
+        <?php $this->load->view('dashboard/_partials/footer');?>
+    </div>
 
+    <?php $this->load->view('dashboard/_partials/js');?>
 </body>
 
 </html>
